5204dab7ebdc64eeaac232ab8f56fca7b6bfa0b2
Cxlopez <cristian4402@hotmail.com>
2022-10-05 23:30:24 -0400

    add modal to maps

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,42 +1,32 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-
+import SideBarItem from './SideBarItem';
 
 export default function SideBar() {
-  const [markers, setMarkers] = useState([]);
+  //used for modal
+  
+
+
+  //shops for side bar
+  const [shops, setShops] = useState([]);
 
   useEffect(() => {
     axios.get('http://localhost:8000/api/barbershops')
-    .then((res) => {
-      setMarkers(res.data.barbershops);
-      
-    })
-  },[])
+      .then((res) => {
+        setShops(res.data.barbershops);
+
+      })
+  }, [])
 
   return (
     <div className='column'>
       {/* map side bar */}
-    {
-      markers.map(marker => (
-        <div>
-          <hr 
-          style={{
-            color: '#000000',
-            backgroundColor: '#bcb5b5',
-            height: 0.1,
-            // borderColor: '#bcb5b5'
-          }}
-          />
-          <img className='side-barber-img' src={marker.cover_photo_url} alt="barbers" /> <br />
-          <p className='sidebar-shop-name'>{`${marker.name}`}</p>
-          <p className='address-text'> {`${marker.street}, ${marker.city}, ${marker.province}, ${marker.post_code}`} </p>
-          <div className='side-btn-container'>
-          <button className='side-book-btn'>Book</button> {marker.open_hours}
-          </div>
-        </div>
-      ))
-    }
+      {
+        shops.map(shop => (
+          <SideBarItem shop={shop} />
+        ))
+      }
     </div>
   )
 
