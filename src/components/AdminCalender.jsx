5daab0cb16fee23e72d07a7d46509e88eef32ebe
Cxlopez <cristian4402@hotmail.com>
2022-10-10 22:12:30 -0400

    fetch barbershop id for admin

--- src/components/AdminCalender.jsx
+++ src/components/AdminCalender.jsx
@@ -1,49 +1,45 @@
 import '../styles/AdminCalender.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 
 export function AdminCalender() {
+  const [appointments, setAppointments] = useState([]);
+  const barbershopId = 1;
 
-  
+  useEffect(() => {
+    axios.get(`http://localhost:8000/api/appointments?barbershop_id=${barbershopId}`)
+      .then((res) => {
+        setAppointments(res.data.appointments);
+      })
+  }, [])
+
   return (
     <div className="calendar dark">
-    <div className="calendar_header">
-      <h1 className = "header_title">Welcome Back</h1>
-      <p className="header_copy"> Calendar Plan</p>
-    </div>
-    <div className="calendar_plan">
-      <div className="cl_plan">
-        <div className="cl_title">Today</div>
-        <div className="cl_copy">22nd  April  2018</div>
+      <div className="calendar_header">
+        <h1 className="header_title">Appointments</h1>
+        <p className="header_copy"> Calendar Plan</p>
+      </div>
+      <div className="calendar_plan">
+        <div className="cl_plan">
+          <div className="cl_title">Today</div>
+          <div className="cl_copy">22nd  April  2018</div>
+        </div>
+      </div>
+      <div className="calendar_events">
+        <p className="ce_title">Upcoming appointments</p>
+        {appointments.map((appointment, index) => (
+          <div key={index} className="event_item">
+            <div className="ei_Dot dot_active"></div>
+            <ul className="ei_Title">
+              <li>
+                {appointment.start_time}
+              </li>
+            </ul>
+            <div className="ei_Copy">{appointment.name} {appointment.last_name}</div>
+          </div>
+        ))}
       </div>
     </div>
-    <div className="calendar_events">
-      <p className="ce_title">Upcoming Events</p>
-      <div className="event_item">
-        <div className="ei_Dot dot_active"></div>
-        <div className="ei_Title">10:30 am</div>
-        <div className="ei_Copy">Monday briefing with the team</div>
-      </div>
-      <div className="event_item">
-        <div className="ei_Dot"></div>
-        <div className="ei_Title">12:00 pm</div>
-        <div className="ei_Copy">Lunch for with the besties</div>
-      </div>
-      <div className="event_item">
-        <div className="ei_Dot"></div>
-        <div className="ei_Title">13:00 pm</div>
-        <div className="ei_Copy">Meet with the client for final design </div>
-      </div>
-      <div className="event_item">
-        <div className="ei_Dot"></div>
-        <div className="ei_Title">14:30 am</div>
-        <div className="ei_Copy">Plan event night to inspire students</div>
-      </div>
-      <div className="event_item">
-        <div className="ei_Dot"></div>
-        <div className="ei_Title">15:30 am</div>
-        <div className="ei_Copy">Add some more events to the calendar</div>
-      </div>
-    </div>
-  </div>
   )
 };
